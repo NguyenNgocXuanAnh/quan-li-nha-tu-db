73b274362699955f96a09af6f7aeef8d9ffb96f0
Võ Trần Gia Hưng <votrangiahung06@gmail.com>
2026-05-16 20:30:19 +0700

    tạo file nhiệm vụ riêng

---
+++ database/votrangiahung.sql
@@ -0,0 +1,12 @@
+﻿--3. Truy vấn: Truy vấn với mệnh đề having (5 câu), Truy vấn sử dụng phép chia (4 câu)
+--4. 1 thủ tục, 2 hàm, 2 trigger
+--5. Tạo 1 người dùng và cấp quyền 
+
+USE QLNT;
+
+--Truy vấn với mệnh đề having (5 câu)
+--Truy vấn sử dụng phép chia (4 câu)
+--Thủ tục (1 câu)
+--Hàm (2 câu)
+--Trigger (2 câu)
+--Tạo 1 người dùng và cấp quyền
