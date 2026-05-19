917e5df0b9843e8194cd38f754c403ab1eb93687
Nguyễn Ngọc Xuân Ánh <xuananhc92324@gmail.com>
2026-05-20 00:14:46 +0700

    update

--- source/QLNT_Winform/UC_DanhMuc.cs
+++ source/QLNT_Winform/UC_DanhMuc.cs
@@ -1,23 +1,53 @@
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace QLNT_Winform
 {
     public partial class UC_DanhMuc : UserControl
     {
+        private DatabaseHelper db = new DatabaseHelper();
+        private TabControl tabControl;
+        private DataGridView dgvToiDanh;
+        private DataGridView dgvCongViec;
+        private DataGridView dgvTaiKhoan;
+
         public UC_DanhMuc()
         {
             InitializeComponent();
+            LoadData();
         }
 
         private void InitializeComponent()
         {
-            Label lbl = new Label();
-            lbl.Text = "Danh Mục Hệ Thống (To be implemented)";
-            lbl.AutoSize = true;
-            lbl.Location = new System.Drawing.Point(50, 50);
-            lbl.Font = new System.Drawing.Font("Segoe UI", 16F);
-            this.Controls.Add(lbl);
+            this.tabControl = new System.Windows.Forms.TabControl();
+            
+            TabPage tabToiDanh = new TabPage("Tội Danh");
+            TabPage tabCongViec = new TabPage("Công Việc");
+            TabPage tabTaiKhoan = new TabPage("Tài Khoản");
+
+            this.dgvToiDanh = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false };
+            this.dgvCongViec = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false };
+            this.dgvTaiKhoan = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false };
+
+            tabToiDanh.Controls.Add(dgvToiDanh);
+            tabCongViec.Controls.Add(dgvCongViec);
+            tabTaiKhoan.Controls.Add(dgvTaiKhoan);
+
+            this.tabControl.Controls.Add(tabToiDanh);
+            this.tabControl.Controls.Add(tabCongViec);
+            this.tabControl.Controls.Add(tabTaiKhoan);
+            
+            this.tabControl.Dock = DockStyle.Fill;
+            this.Controls.Add(this.tabControl);
             this.Size = new System.Drawing.Size(800, 600);
+        }
+
+        private void LoadData()
+        {
+            dgvToiDanh.DataSource = db.ExecuteQuery("SELECT * FROM TOIDANH");
+            dgvCongViec.DataSource = db.ExecuteQuery("SELECT * FROM CONGVIEC");
+            dgvTaiKhoan.DataSource = db.ExecuteQuery("SELECT * FROM TAIKHOAN");
         }
     }
 }
--- source/QLNT_Winform/UC_ThongKe.cs
+++ source/QLNT_Winform/UC_ThongKe.cs
@@ -1,23 +1,56 @@
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace QLNT_Winform
 {
     public partial class UC_ThongKe : UserControl
     {
+        private DatabaseHelper db = new DatabaseHelper();
+        private TabControl tabControl;
+        private DataGridView dgvPhongGiam;
+        private DataGridView dgvToiDanh;
+        private DataGridView dgvViPham;
+
         public UC_ThongKe()
         {
             InitializeComponent();
+            LoadData();
         }
 
         private void InitializeComponent()
         {
-            Label lbl = new Label();
-            lbl.Text = "Thống Kê & Báo Cáo (To be implemented)";
-            lbl.AutoSize = true;
-            lbl.Location = new System.Drawing.Point(50, 50);
-            lbl.Font = new System.Drawing.Font("Segoe UI", 16F);
-            this.Controls.Add(lbl);
+            this.tabControl = new System.Windows.Forms.TabControl();
+            
+            TabPage tabPhongGiam = new TabPage("Hiệu Suất Phòng Giam");
+            TabPage tabToiDanh = new TabPage("Thống Kê Tội Danh");
+            TabPage tabViPham = new TabPage("Thống Kê Vi Phạm");
+
+            this.dgvPhongGiam = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false };
+            this.dgvToiDanh = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false };
+            this.dgvViPham = new DataGridView() { Dock = DockStyle.Fill, ReadOnly = true, AllowUserToAddRows = false };
+
+            tabPhongGiam.Controls.Add(dgvPhongGiam);
+            tabToiDanh.Controls.Add(dgvToiDanh);
+            tabViPham.Controls.Add(dgvViPham);
+
+            this.tabControl.Controls.Add(tabPhongGiam);
+            this.tabControl.Controls.Add(tabToiDanh);
+            this.tabControl.Controls.Add(tabViPham);
+            
+            this.tabControl.Dock = DockStyle.Fill;
+            this.Controls.Add(this.tabControl);
             this.Size = new System.Drawing.Size(800, 600);
+        }
+
+        private void LoadData()
+        {
+            // Call SQL Functions
+            dgvPhongGiam.DataSource = db.ExecuteQuery("SELECT * FROM dbo.fn_PHONGGIAM_ThongKe()");
+            dgvToiDanh.DataSource = db.ExecuteQuery("SELECT * FROM dbo.fn_TOIDANH_ThongKe()");
+            
+            // Call SQL Procedure
+            dgvViPham.DataSource = db.ExecuteStoredProcedureQuery("sp_ThongKeViPhamTheoHinhThuc");
         }
     }
 }
